70dc7402cbd684a12616c20e48c33f57688bd279
vedgharat <gved.16@gmail.com>
2026-02-15 13:53:46 +0530

    2nd one

---
+++ leetcode/LC_67.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string addBinary(string a,string b){
+    string res="";
+    int i=a.size()-1,j=b.size()-1,carry=0;
+    while(i>=0||j>=0||carry){
+        int sum=carry;
+        if(i>=0) sum+=a[i--]-'0';
+        if(j>=0) sum+=b[j--]-'0';
+        res.push_back('0'+(sum%2));
+        carry=sum/2;
+    }
+    reverse(res.begin(),res.end());
+    return res;
+}
+
+};
